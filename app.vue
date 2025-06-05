f55e6c8266ee4aa21b038ad8944d25102a6178f5
Alex <53570832+AlexFrontWeb@users.noreply.github.com>
2025-06-05 17:58:52 +0200

    Font added

--- app.vue
+++ app.vue
@@ -7,4 +7,17 @@
   </div>
 </template>
 
+<script setup>
+useHead({
+  link: [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap",
+    },
+  ],
+});
+</script>
+
 <style lang="scss"></style>
