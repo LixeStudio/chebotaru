a757a7b4c0e425ecf0368649e53dc854cc3d8da5
Alex <53570832+AlexFrontWeb@users.noreply.github.com>
2025-06-25 19:58:06 +0200

    Added new styles for hover in article-card, wrapped article-card component in NuxtLink.

--- components/BlogArticle.vue
+++ components/BlogArticle.vue
@@ -1,36 +1,39 @@
 <template>
   <article class="articles-blog__article article-card">
-    <div class="articles-blog__image article-card__image">
-      <NuxtImg
-        v-if="article.image && article.image.src"
-        :src="article.image.src"
-        :alt="article.image.alt"
-        width="392"
-        height="300"
-      />
-    </div>
-    <h3 class="articles-blog__title article-card__title">
-      {{ article.title }}
-    </h3>
-    <p class="articles-blog__text article-card__text">
-      {{ article.previewText }}
-    </p>
-    <span class="articles-blog__separator-line article-card__separator-line" />
-    <div class="articles-blog__bottom article-card__bottom">
-      <p class="articles-blog__theme article-card__theme">
-        {{ themeValues[article.theme] }}
+    <NuxtLink
+      :to="localePath(`/article/${article.documentId}-${article.slug}`)"
+    >
+      <div class="articles-blog__image article-card__image">
+        <NuxtImg
+          v-if="article.image && article.image.src"
+          :src="article.image.src"
+          :alt="article.image.alt"
+          width="392"
+          height="300"
+        />
+      </div>
+      <h3 class="articles-blog__title article-card__title">
+        {{ article.title }}
+      </h3>
+      <p class="articles-blog__text article-card__text">
+        {{ article.previewText }}
       </p>
-      <p
-        v-if="article.publishedAt"
-        class="articles-blog__date article-card__date"
-      >
-        {{ formatDate(article.publishedAt, locale) }}
-      </p>
-      <NuxtLink
-        :to="localePath(`/article/${article.documentId}-${article.slug}`)"
-        class="articles-blog__linkto article-card__linkto link-to"
+      <span
+        class="articles-blog__separator-line article-card__separator-line"
       />
-    </div>
+      <div class="articles-blog__bottom article-card__bottom">
+        <p class="articles-blog__theme article-card__theme">
+          {{ themeValues[article.theme] }}
+        </p>
+        <p
+          v-if="article.publishedAt"
+          class="articles-blog__date article-card__date"
+        >
+          {{ formatDate(article.publishedAt, locale) }}
+        </p>
+        <div class="articles-blog__linkto article-card__linkto link-to" />
+      </div>
+    </NuxtLink>
   </article>
 </template>
 
